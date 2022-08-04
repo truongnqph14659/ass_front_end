ac2c2e82a15f05224a9746de11e2e4bad63bebb1
= <huynguyenz2002@gmail.com>
2022-08-05 00:23:25 +0700

    list order

---
+++ src/components/admin/orderList.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const ListOrder = {
+  async render(){
+        const {data} =await axios.get('http://localhost:8080/api/Order')
+        console.log(data);
+        let index = 1
+        return/*html*/`
+            <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Stt</th>
+                <th scope="col">Mã đơn</th>
+                <th scope="col">Note</th>
+                <th scope="col">view</th>
+            </tr>
+            </thead>
+            <tbody>
+            ${data.map((item:any)=>{
+                return /*html*/`
+                    <tr>  
+                        <th scope="row">${index++}</th>
+                        <td>${item.name}</td>
+                        <td>${item.note}</td>
+                        <td>
+                            <button type="button" class="btn btn-success bg-[#198754]">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                `
+            }).join('')}
+            </tbody>
+        </table>
+        `
+    }
+}
+
+export default ListOrder
