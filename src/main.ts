ccc534738c088d607397df617b1bc0d1562d2469
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-08-04 23:20:28 +0700

    list product page #3

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Navigo from 'navigo';
 import HomePage from './pages/Home'
 import AdminPage from './pages/Admin';
 import DetailPage from './pages/Detail';
+import AdminListProductPage from './pages/AdminListProduct';
 const router = new Navigo('/', {linksSelector: "a"})
 type ComponentBase = {
   render: (id:number) => Promise<string> | string,
@@ -19,7 +20,8 @@ const print =async (component: ComponentBase, id?:any) => {
 router.on({
   "/": async () => print(await HomePage),
   "/detail": () => print(DetailPage),
-  '/admin':()=>print(AdminPage),
+  '/admin':()=> print(AdminPage),
+  "/admin/productList": () => print(AdminListProductPage)
 
 })
 router.resolve()
